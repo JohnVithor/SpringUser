2e75b189eb3a330750682e987309b71c74818b00
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-12 11:19:03 -0300

    adiciona novo tratamento de erro

--- src/main/java/ufrn/imd/jv/springuser/RestExceptionHandler.java
+++ src/main/java/ufrn/imd/jv/springuser/RestExceptionHandler.java
@@ -1,12 +1,16 @@
 package ufrn.imd.jv.springuser;
 
+import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.io.IOException;
 
 /**
  * Classe de manipulação dos erros lançados e construção de uma resposta mais
@@ -18,5 +22,9 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handlerException(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @ExceptionHandler(ClientAbortException.class)
+    public void handleClientAbortException(ClientAbortException e) {
+        System.err.println(e.getMessage());
     }
 }
